ac31c63cd76475e8c526d7c0c43e444fbe14e98d
Rafal Enden <git@enden.com>
2025-07-11 13:26:15 +0200

    Add GH and LI links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,23 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="home-page">
-          <header className="header">
-      <h1>EndCode Ltd</h1>
-      <h2>Rafal Enden</h2>
-      <p>3rd Floor, 86-90 Paul Street, London, England, EC2A 4NE</p>
-      <p>VAT Number: GB248764465</p>
-      <div className="links">
-        <EmailLink user="site-contact">Contact</EmailLink>
-        <Link href="/privacy-policy">Privacy Policy</Link>
-        <Link href="/terms-of-service">Terms of Service</Link>
-      </div>
-    </header> 
+      <header className="header">
+        <h1>EndCode Ltd</h1>
+        <h2>Rafal Enden</h2>
+        <p>3rd Floor, 86-90 Paul Street, London, England, EC2A 4NE</p>
+        <p>VAT Number: GB248764465</p>
+        <div className="links">
+          <EmailLink user="site-contact">Contact</EmailLink>
+          <Link href="https://link.enden.com/github?from=site">GitHub</Link>
+          <Link href="https://link.enden.com/linkedin?from=site">LinkedIn</Link>
+        </div>
+      </header>
+      <footer className="footer">
+        <div className="footer-links">
+          <Link href="/privacy-policy">Privacy Policy</Link>
+          <Link href="/terms-of-service">Terms of Service</Link>
+        </div>
+      </footer>
     </main>
   );
 }
